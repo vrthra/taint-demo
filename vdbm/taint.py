9eb50f214dc5178b27ba7e4945441b31091037f9
Rahul Gopinath <rahul@gopinath.org>
2017-12-19 12:16:05 -0800

    update

--- vdbm/taint.py
+++ vdbm/taint.py
@@ -18,16 +18,23 @@ class tstr(str):
 def mark(module):
     for name in dir(module):
         obj = getattr(module, name)
-        if isinstance(obj, types.FunctionType) or isinstance(obj, types.BuiltinFunctionType):
+        if isinstance(obj, (types.FunctionType, types.BuiltinFunctionType)):
             yield (module, name, obj)
+
+# Sink
 
 def sink(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
-        for e in (list(args) + list(kwargs.values())):
-            if isinstance(e, tstr): raise Exception("tainted")
+        if any(isinstance(e, tstr) for e in list(args) + list(kwargs.values())):
+            raise Exception("tainted")
         return func(*args, **kwargs)
     return wrapper
+
+def mark_sinks(module):
+    for (module, name, obj) in mark(module): setattr(module, name, sink(obj))
+
+# Sanitizer
 
 def sanitizer(func):
     @functools.wraps(func)
@@ -37,34 +44,25 @@ def sanitizer(func):
         return func(*my_args, **my_kwargs)
     return wrapper
 
-def mark_sinks(module):
-    for (module, name, obj) in mark(module):
-        setattr(module, name, sink(obj))
+# Source
 
-def make_strtuple_wrapper(fun):
-    def proxy(*a, **kw): return tuple(tstr(l) for l in fun(*a, **kw))
-    return 
+class ProxyIter(collections.Iterator):
+    def __init__(self, i): self.i = i
 
-def make_strlst_wrapper(fun):
-    def proxy(*a, **kw): return [tstr(l) for l in fun(*a, **kw)]
-    return proxy
+    def __iter__(self): return self
 
-def make_str_wrapper(fun):
-    def proxy(*a, **kw): return tstr(fun(*a, **kw))
-    return proxy
+    def __enter__(self): return self
 
-for name, fn in inspect.getmembers(str, callable):
-    tuple_names = ['partition', 'rpartition']
-    bool_names = ['__eq__', '__lt__', '__gt__', '__contains__']
-    list_names = ['rsplit', 'splitlines', 'split']
-    repr_names =  ['__repr__', '__str__', '__hash__']
-    if name not in ['__class__', '__new__', '__init__', '__getattribute__',
-            '__init_subclass__', '__subclasshook__', '__setattr__',
-            '__len__', 'find', 'rfind', '__iter__'
-            ] + tuple_names + list_names + bool_names + repr_names:
-        setattr(tstr, name, make_str_wrapper(fn))
-    elif name in list_names: setattr(tstr, name, make_strlst_wrapper(fn))
-    elif name in tuple_names: setattr(tstr, name, make_strtuple_wrapper(fn))
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        return self.i.__exit__(exc_type, exc_val, exc_tb)
+
+    def __next__(self): return tstr(self.i.__next__())
+
+    def __hasattr__(self, name): return hasattr(self.i, name)
+
+    def __getattr__(self, name):
+        func = getattr(self.i,name)
+        return source(func) if func else None
 
 def source(func):
     @functools.wraps(func)
@@ -87,30 +85,29 @@ def source(func):
     return wrapper
 
 def mark_sources(module):
-    for (module, name, obj) in mark(module):
-        setattr(module, name, source(obj))
+    for (module, name, obj) in mark(module): setattr(module, name, source(obj))
 
-class ProxyIter(collections.Iterator):
-    def __init__(self, i):
-        self.i = i
+# Propagation
 
-    def __iter__(self):
-        return self
+def make_strtuple_wrapper(fun):
+    return lambda *a, **kw: tuple(tstr(l) for l in fun(*a, **kw))
 
-    def __enter__(self):
-        return self
+def make_strlst_wrapper(fun):
+    return lambda *a, **kw: [tstr(l) for l in fun(*a, **kw)]
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        return self.i.__exit__(exc_type, exc_val, exc_tb)
+def make_str_wrapper(fun):
+    return lambda *a, **kw: tstr(fun(*a, **kw))
 
-    def __next__(self):
-        return tstr(self.i.__next__())
-
-    def __hasattr__(self, name):
-        return hasattr(self.i, name)
-
-    def __getattr__(self, name):
-        func = getattr(self.i,name)
-        if not func: return None
-        return source(func)
+for name, fn in inspect.getmembers(str, callable):
+    tuple_names = ['partition', 'rpartition']
+    bool_names = ['__eq__', '__lt__', '__gt__', '__contains__']
+    list_names = ['rsplit', 'splitlines', 'split']
+    repr_names =  ['__repr__', '__str__', '__hash__']
+    if name not in ['__class__', '__new__', '__init__', '__getattribute__',
+            '__init_subclass__', '__subclasshook__', '__setattr__',
+            '__len__', 'find', 'rfind', '__iter__'
+            ] + tuple_names + list_names + bool_names + repr_names:
+        setattr(tstr, name, make_str_wrapper(fn))
+    elif name in list_names: setattr(tstr, name, make_strlst_wrapper(fn))
+    elif name in tuple_names: setattr(tstr, name, make_strtuple_wrapper(fn))
 
